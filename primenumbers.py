ce1d4c17bbd5f0466b685ae89f1f478139c0545d
Yeswanth Karthik <122425093+yeswanth-karthik@users.noreply.github.com>
2023-01-11 11:34:14 +0530

    Update primenumbers.py

--- primenumbers.py
+++ primenumbers.py
@@ -3,7 +3,7 @@ for i in range(1,n+1):
      sum=0
      for j in range(1,n+1):
           if((i%j)==0):
-             sum=sum+1
-      if(sum==2):
-         print(i)
+               sum=sum+1
+     if(sum==2):
+          print(i)
          
